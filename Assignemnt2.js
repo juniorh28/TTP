95abc0d3487da087df7ace6542225d1df4e218e6
juniorh28 <juniorh2897@gmail.com>
2021-01-06 12:46:44 -0500

    listed all the function we must change

--- Assignemnt2.js
+++ Assignemnt2.js
@@ -1,1 +1,7 @@
 //Assignment 2 is recreating native javascript functions without using the them.
+
+//forEach() method will iterate through the array without mutating it.
+
+function myEach(array) {
+  for (let i = 0; i < array.length; i++) {}
+}
