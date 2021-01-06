a468cbe70bfd904651ce596f3e2a9acd41a06c7a
juniorh28 <juniorh2897@gmail.com>
2021-01-06 13:38:20 -0500

    added custom map function

--- Assignemnt2.js
+++ Assignemnt2.js
@@ -1,8 +1,10 @@
 //Assignment 2 is recreating native javascript functions without using the them.
 
+const arr = [1, 2, 3, 4, 5, 6];
+
 //forEach() method will iterate through the array without mutating it.
 //1. Create a forEach function called myEach with callback.
-
+console.log("this is part 1");
 function myEach(array, cb) {
   for (let i = 0; i < array.length; i++) {
     cb(array[i]);
@@ -12,12 +14,28 @@ const cb = (arg) => {
   console.log(arg);
 };
 
-const arr = [1, 2, 3, 4, 5, 6];
 myEach(arr, cb);
 
 //2.map()
+console.log("\nthis is part 2");
+function myMap(arr, cb2) {
+  const returnArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    returnArr.push(cb2(element));
+    console.log(returnArr[i]);
+  }
+  return returnArr;
+}
+
+const cb2 = (args) => {
+  return args * 2;
+};
+
+myMap(arr, cb2);
 
 //3.filter()
+console.log("\nthis is part 3");
 
 //4.some()
 
